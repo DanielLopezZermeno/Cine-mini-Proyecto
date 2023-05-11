29329f0824c0fc3797898c1f2f91279e67b55c63
DanielLopezZermeno <86745921+DanielLopezZermeno@users.noreply.github.com>
2023-05-11 12:10:29 -0600

    Componente Reservar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { CarteleraComponent } from './cartelera/cartelera.component';
 import { AlimentosComponent } from './alimentos/alimentos.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { UbicanosComponent } from './ubicanos/ubicanos.component';
+import { ReservarComponent } from './reservar/reservar.component';
 const routes: Routes = [
   { path: '', redirectTo: '/cartelera', pathMatch: 'full' },
   { path: 'cartelera', component: CarteleraComponent },
   { path: 'alimentos', component: AlimentosComponent },
   { path: 'ubicanos', component: UbicanosComponent },
   { path: 'perfil', component: PerfilComponent },
+  { path: 'reservar', component: ReservarComponent}
 ];
 
 @NgModule({
